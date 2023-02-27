81481fecc0c8c4720cd23f37ccfde50c9cc30380
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-27 16:21:30 +0100

    FEAT mounted list

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 console.log('ciao')
 
 const { createApp } = Vue
-
+console.log(axios)
 createApp({
     data() {
         return {
             message: 'Hello Vue!',
             emails: [],
-            emailsObjs: {},
         }
     },
-    methods:{
-        repeatName() {
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then ((res)=> {
-                console,log(res)
-            })
+    methods: {
+        repeatEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    console.log(res.data)
+                    this.emails.push(res.data.response)
+                })
         }
-    }
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.repeatEmail()
+        }
+    },
 }).mount('#app')
 
