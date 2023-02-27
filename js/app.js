c3701412790bacc274e4a3cb24799603698c2fcb
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-27 18:07:59 +0100

    BONUS view email all togheter

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
 console.log('ciao')
 
 const { createApp } = Vue
-console.log(axios)
+// console.log(axios)
 createApp({
     data() {
         return {
             message: 'Hello Vue!',
             emails: [],
         }
+    
     },
     methods: {
         repeatEmail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((res) => {
-                    console.log(res.data)
-                    this.emails.push(res.data.response)
+                        this.emails.push(res.data.response)
                 })
         }
     },
     mounted() {
         for (let i = 0; i < 10; i++) {
-            this.repeatEmail()
+           this.repeatEmail()
         }
     },
 }).mount('#app')
+const lunghezza = this.emails
+// console.log(lunghezza)
 
